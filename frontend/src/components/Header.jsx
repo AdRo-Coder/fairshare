e5033284f5e6147b85e4e016a7d7bded661cf12a
Adrian Rohrbach <adrian.rohrbach@stud.hslu.ch>
2026-08-19 11:15:53 +1200

    feat: added logo to UI to navigate back to the landing page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,13 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import FairShareLogo from '../resources/FairShareLogo.png';
 
 function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    setIsLoggedIn(Boolean(localStorage.getItem('fairshareUser')));
-  }, []);
-
   return (
     <header className="header">
       <div className="container header-inner">
-        <Link to="/" className="logo">FairShare</Link>
-        <nav className="main-nav">
-          <Link to="/">Home</Link>
-          <Link to="/groups">Groups</Link>
-          {isLoggedIn ? (
-            <Link to="/profile">Profile</Link>
-          ) : (
-            <Link to="/login">Log In</Link>
-          )}
-          <Link to="/groups/new">Create Group</Link>
-        </nav>
+        <Link to="/" className="logo" aria-label="FairShare home">
+          <img src={FairShareLogo} alt="FairShare logo" className="logo-image" />
+        </Link>
       </div>
     </header>
   );
