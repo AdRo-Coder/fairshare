1103e9f9a27a77a7ff10d8c4a7d31e749e5c55f3
Adrian Rohrbach <adrian.rohrbach@stud.hslu.ch>
2026-08-19 10:42:47 +1200

    feat: navigation for the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,50 @@
-﻿import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+﻿import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import UserProfile from './UserProfile.jsx';
 import GroupsOverview from './pages/GroupsOverview.jsx';
 import CreateGroup from './pages/CreateGroup.jsx';
 import GroupPage from './pages/GroupPage.jsx';
-import Header from './components/Header.jsx';
 import Landing from './pages/Landing.jsx';
 import GroupMembers from './pages/GroupMembers.jsx';
 import './index.css';
+import App from './App.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        children: [
+            {
+                path: '/',
+                element: <Landing/>,
+            },
+            {
+                path: '/register',
+                element: <UserProfile/>,
+            },
+            {
+                path: 'groups',
+                element: <GroupsOverview/>,
+            },
+            {
+                path: '/groups/new',
+                element: <CreateGroup/>,
+            },
+            {
+                path: '/groups/:id',
+                element: <GroupPage/>,
+            },
+            {
+                path: '/groups/:id/members',
+                element: <GroupMembers/>
+            }
+        ]
+    }
+]);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/register" element={<UserProfile />} />
-                <Route path="/groups" element={<GroupsOverview />} />
-                <Route path="/groups/new" element={<CreateGroup />} />
-                <Route path="/groups/:id" element={<GroupPage />} />
-                <Route path="/groups/:id/members" element={<GroupMembers />} />
-            </Routes>
-        </BrowserRouter>
+        <RouterProvider router={router}/>
     </StrictMode>,
 );
