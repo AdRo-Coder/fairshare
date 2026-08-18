15c4663fccdb00f63c62d9d17a716de5d866c36d
Adrian Rohrbach <adrian.rohrbach@stud.hslu.ch>
2026-08-19 11:15:53 +1200

    feat: added logo to UI to navigate back to the landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import Header from './components/Header.jsx';
 
 function App() {
     return (
-        <div>
+        <>
+            <Header />
             <Outlet />
-        </div>
+        </>
     );
 }
 
